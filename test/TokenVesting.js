4f9f120afb968c41fa3db8d51f70f5cd1202f959
rcimoszko <ryan.cimoszko@gmail.com>
2017-10-19 10:56:46 -0700

    restructure tests for vesting token

--- test/TokenVesting.js
+++ test/TokenVesting.js
@@ -2,76 +2,61 @@ const FansUniteToken = artifacts.require('FansUniteToken.sol');
 const FansUniteCrowdfund = artifacts.require('FansUniteCrowdfund.sol');
 const TokenVesting = artifacts.require('TokenVesting.sol');
 const Utils = require('./helpers/Utils.js');
+const BigNumber = web3.BigNumber;
 
 let owner;
 let beneficiary;
-
-let vestingAddress;
-let incentivisationAddress;
-let platformSupplyAddress;
-let unsoldSupplyAddress;
-
-let buyerAddress;
-
-let crowdfund;
 
 let maxSupply;
 
 let token;
 let vesting;
-let duration;
 let start;
+let amount;
 
-const oneWeekInSeconds = 604800;
 
 contract('TokenVesting', (accounts) => {
 
-    before(async()=> {
+    beforeEach(async()=> {
+        amount = new BigNumber(1000);
+
         owner = accounts[0];
+        maxSupply = 700 * 10**24;
         beneficiary = accounts[1];
 
-        vestingAddress = accounts[2];
-        platformSupplyAddress = accounts[3];
-        incentivisationAddress = accounts[4];
-        unsoldSupplyAddress = accounts[5];
+        token = await FansUniteToken.new('FansUnite Token', 'FAN', maxSupply, { from: owner });
 
-        buyerAddress = accounts[6];
-        maxSupply = 700 * 10**24;
-        duration = 72 * oneWeekInSeconds;
-        start = 1512550800;
+        start = Utils.latestTime() + Utils.duration.minutes(1); // +1 minute so it starts after contract instantiation
+        duration = Utils.duration.years(2);
 
-        token = await FansUniteToken.new('FansUnite Token', 'FAN', maxSupply, {from: owner});
+        vesting = await TokenVesting.new(beneficiary, start, duration, { from: owner });
 
-        vesting = await TokenVesting.new(beneficiary, 1512550800, duration, { from: owner });
-
-        crowdfund = await FansUniteCrowdfund.new(
-            beneficiary,
-            token.address,
-            vesting.address,
-            incentivisationAddress,
-            platformSupplyAddress,
-            unsoldSupplyAddress,
-            {from: owner}
-        );
-        token.transferOwnership(crowdfund.address);
-
-        await Utils.addSeconds((await crowdfund.endsAt() - web3.eth.getBlock('latest').timestamp));
-        await crowdfund.finalize({from: owner});
+        await token.mint(vesting.address, amount, { from: owner });
 
     });
 
 
     it('should linearly release tokens during vesting period', async function () {
-        const checkpoints = 6;
+        const vestingPeriod = duration;
+        const checkpoints = 4;
 
         for (let i = 1; i <= checkpoints; i++) {
-            const now = start + i * (duration / checkpoints);
+            const now = start + i * (vestingPeriod / checkpoints);
             await Utils.increaseTimeTo(now);
 
             await vesting.release(token.address);
             const balance = await token.balanceOf(beneficiary);
-            const expectedVesting = (amount * (now - start) / duration);
-            assert.equal(Utils.convertFromDecimals(balance), expectedVesting);
+            const expectedVesting = amount.mul(now - start).div(duration).floor();
+
+            assert.equal(expectedVesting.toNumber(), balance.toNumber());
         }
     });
+
+    it('should have released all after end', async function () {
+        await Utils.increaseTimeTo(start + duration);
+        await vesting.release(token.address);
+        const balance = await token.balanceOf(beneficiary);
+        assert.equal(balance.toNumber(), amount.toNumber());
+    });
+
 });
